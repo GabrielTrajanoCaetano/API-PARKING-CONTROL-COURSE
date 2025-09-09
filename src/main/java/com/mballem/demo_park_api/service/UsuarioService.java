f8a72494e543812cc5259e60e7beb3b264cb1087
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-09-09 17:15:27 -0300

    finish create users endpoint

--- src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
+++ src/main/java/com/mballem/demo_park_api/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package com.mballem.demo_park_api.service;
 
+import com.mballem.demo_park_api.entity.Usuario;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import repository.UsuarioRepository;
 
 @RequiredArgsConstructor
@@ -11,4 +13,9 @@ public class UsuarioService {
 
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
