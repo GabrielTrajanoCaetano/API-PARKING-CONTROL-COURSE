bc699a0f3fdffe1af31c6ce4dccb755960c8c5fa
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-09-18 22:17:03 -0300

    working at security

---
+++ src/main/java/com/mballem/demo_park_api/jwt/JwtToken.java
@@ -0,0 +1,14 @@
+package com.mballem.demo_park_api.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtToken {
+    private String token;
+}
