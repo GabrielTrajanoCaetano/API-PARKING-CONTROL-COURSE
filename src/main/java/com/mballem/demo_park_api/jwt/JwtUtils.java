248d3c0f543a0c7ea0b37c9de646b1f5e8c345eb
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-09-24 16:55:35 -0300

    finisheded the configuration of permission access to client and admin

--- src/main/java/com/mballem/demo_park_api/jwt/JwtUtils.java
+++ src/main/java/com/mballem/demo_park_api/jwt/JwtUtils.java
@@ -22,7 +22,7 @@ public class JwtUtils {
     public static final String SECRET_KEY = "E12345678-0123456789-B1326547890";
     public static final long EXPIRE_DAYS = 0;
     public static final long EXPIRE_HOURS = 0;
-    public static final long EXPIRE_MINUTES = 2;
+    public static final long EXPIRE_MINUTES = 30;
 
     private JwtUtils(){
 
