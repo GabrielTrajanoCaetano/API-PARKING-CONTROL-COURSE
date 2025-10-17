92aafdfdc0c95871c3a40baee06905f466587d99
Gabriel Trajano Caetano <gabriel.caetanotrajano@hotmail.com>
2025-10-17 15:35:20 -0300

    method of gerarRecibo created

--- src/main/java/com/mballem/demo_park_api/web/dto/mapper/ClienteVagaMapper.java
+++ src/main/java/com/mballem/demo_park_api/web/dto/mapper/ClienteVagaMapper.java
@@ -1,8 +1,22 @@
 package com.mballem.demo_park_api.web.dto.mapper;
 
+import com.mballem.demo_park_api.entity.ClienteVagas;
+import com.mballem.demo_park_api.web.dto.ClienteCreateDto;
+import com.mballem.demo_park_api.web.dto.ClienteResponseDto;
+import com.mballem.demo_park_api.web.dto.EstacionamentoCreateDto;
+import com.mballem.demo_park_api.web.dto.EstacionamentoResponseDto;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClienteVagaMapper {
+
+    public static ClienteVagas toClienteVaga(EstacionamentoCreateDto createDto){
+        return new ModelMapper().map(createDto, ClienteVagas.class);
+    }
+
+    public static EstacionamentoResponseDto toDto(ClienteVagas vagas){
+        return new ModelMapper().map(vagas, EstacionamentoResponseDto.class);
+    }
 }
